cb74f63033893fe08abe6affde0ad7ec6bf9d8b9
조연정 <jojoor201@gmail.com>
2026-02-26 21:40:01 +0900

    day1

---
+++ src/pages/search/index.tsx
@@ -0,0 +1,13 @@
+import { useRouter } from "next/router";
+
+export default function Page() {
+  const router = useRouter();
+  const { q } = router.query;
+
+  console.log(router);
+  return (
+    <>
+      <h1>검색결과 : {q}</h1>
+    </>
+  );
+}
